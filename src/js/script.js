870dbfe208ca4b7ffb4d87b7415ff2f0d695b16f
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-08-14 23:42:59 +0900

    ボイス　アコーディオン

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    $('.box__bottom').on('click', function() {
+    $(this).find('.box__main').next('.box__sub').slideToggle();
+    $(this).find('.box__body').toggleClass('js-open');
+    });
 
 });
